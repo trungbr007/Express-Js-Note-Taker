2b05e8f046cd1b2263cc229ece35c2815329e654
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-05-09 00:37:07 +0700

    test get method

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+const {database} = require('./Develop/db/db.json');
 const express = require('express');
 const app = express();
+
+app.get('/api/database', (req, res) => {
+    res.json(database);
+  }); 
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
+
+ 
