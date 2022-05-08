7c5f18a6c6ce397826d9dfcc9fedbc9b6152a79c
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-05-09 01:11:56 +0700

    test Heroku

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const database = require('./Develop/db/db.json');
 
 //Setting up Server
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/api/database', (req, res) => {
     res.json(database);
   }); 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
 
   app.get('/api/animals', (req, res) => {
