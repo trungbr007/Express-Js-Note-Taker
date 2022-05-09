1fa9e8633b6d70461310ada1ad20adde09a42f20
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-05-09 15:51:07 +0700

    get request

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const database = require('./Develop/db/db.json');
-
-//Setting up Server
 const express = require('express');
+//Setting up Server
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+//API Routes 
 app.get('/api/database', (req, res) => {
     res.json(database);
   }); 
 
-  app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
 
-  app.get('/api/animals', (req, res) => {
+  app.get('/api/database', (req, res) => {
     let results = database;
     console.log(req.query)
     res.json(results);
   });
  
+
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
