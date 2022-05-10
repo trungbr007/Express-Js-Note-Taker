ecd4da16e18acb800a74006010897c902a1606f8
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-05-10 21:04:26 +0700

    Refactor code and create post method for api/notes

--- server.js
+++ server.js
@@ -13,23 +13,51 @@ const app =express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json())
+
 //Set static folder, it will have acces to all the files in the public by changing the endpoints
 app.use(express.static(path.join(__dirname,'./Develop/public')));
-//Filter 
+//Validate notes function
+function validateNote(note) {
+    if (!note.title|| typeof note.title !== 'string') {
+      return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+      return false;
+    }
+    return true;
+  }
 
-
+//Create new Notes
+function createNewNotes(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, './Develop/db/db.json'),
+      JSON.stringify({ note: notesArray }, null, 2)
+    );
+    return note;
+  }
 
 //Create routes/get
 
 app.get('/api/notes', (req, res) => {
-    
     res.json(data);
   });
 
-  
-  
 
 //Create post route
+app.post('/api/notes',(req,res)=>{
+    //set id based on what the next index of the array will be
+    req.body.id = data.note.length.toString();
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The notes is not properly formatted.');
+      } else {
+        const note = createNewNotes(req.body,data.note);
+        res.json(note);
+      }
+    });
+    
 
   
 //Delete Route 
