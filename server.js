a050b0df24f0e78677118b88e5def252caf22ccd
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-05-10 10:49:50 +0700

    add note

--- server.js
+++ server.js
@@ -1,23 +1,49 @@
 const database = require('./Develop/db/db.json');
 const express = require('express');
+const path = require ("path");
+const fs = require ("fs");
+const util = require ("util");
+
+
 //Setting up Server
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//Add Middleware So the Application Can Accept POST Data
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
-//API Routes 
-app.get('/api/database', (req, res) => {
-    res.json(database);
-  }); 
+//Add the route Get method
 
+app.get('/api/notes', (req, res) => {
+  res.json(database);
+});
 
-  app.get('/api/database', (req, res) => {
-    let results = database;
-    console.log(req.query)
-    res.json(results);
-  });
+app.post('/api/notes', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = database.length.toString();
+
+  // add animal to json file and animals array in this function
+  const notes = createNewNotes(req.body, database);
+
+  res.json(database);
+});
+
+function createNewNotes(body, notesArray) {
+  const notes = body;
+  notesArray.push(notes);
+  fs.writeFileSync(
+    path.join(__dirname, './Develop/db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return notes;
+}
+
  
 
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
+
