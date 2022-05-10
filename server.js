8f6a30fde27d9435bb2bc2e30aa3c3a50eafa7e7
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-05-10 11:20:58 +0700

    change database variable to notes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const database = require('./Develop/db/db.json');
+const notes = require('./Develop/db/db.json');
 const express = require('express');
 const path = require ("path");
 const fs = require ("fs");
@@ -22,31 +22,41 @@ app.use(express.static("./Develop/public"));
 //the API route Get method
 
 app.get('/api/notes', (req, res) => {
-  res.json(database);
+  res.json(notes);
 });
 
 // API Route Post request
 app.post('/api/notes', (req, res) => {
   
-  req.body = database.length.toString();
+  req.body = notes.length.toString();
 
   // add notes to json file and notes array in this function
-  const notes = createNewNotes(req.body, database);
+  const notes = createNewNotes(req.body, notes);
 
-  res.json(database);
+  res.json(notes);
 });
 
 // create function to add notes and write to file
 function createNewNotes(body, notesArray) {
   const notes = body;
   notesArray.push(notes);
-  fs.writeFileSync(
-    path.join(__dirname, './Develop/db/db.json'),
+
+  fs.writeFileSync(path.join(__dirname, './Develop/db/db.json'),
     JSON.stringify({ notes: notesArray }, null, 2)
   );
   return notes;
 }
 
+//function to validate input
+function validateNote(notes) {
+  if (!notes.title || typeof notes.title !== 'string') {
+      return false;
+  }
+  if (!notes.text || typeof notes.text !== 'string') {
+      return false;
+  }
+  return true;
+}
  
 
   app.listen(PORT, () => {
